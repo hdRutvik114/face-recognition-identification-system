d1d55b5f054e73abe2f1c5b704010c90666c6754
hdRutvik114 <hdrutvik@gmail.com>
2026-09-18 16:30:27 +0530

    This insightface give me the landmark , bounding box and the number of faces detected umm trying to understand this on

--- app/detect.py
+++ app/detect.py
@@ -7,7 +7,7 @@ app.prepare(ctx_id=-1, det_size=(640, 640))
 
 
 image = cv2.imread("image.jpeg")
-
+# This one takes teh image 
 faces = app.get(image)
 
 print(f"Number of faces detected: {len(faces)}")
@@ -16,6 +16,6 @@ for face in faces:
     print("Bounding box:", face.bbox)
     print("Confidence:", face.det_score)
     print("Landmarks:", face.kps)
-
+   
     print("Embedding shape:", face.embedding.shape)
     print("First 5 values:", face.embedding[:5])
