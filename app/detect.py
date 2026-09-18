a5b6103cb1763fa40125edb98a97fa1441dc3498
hdRutvik114 <hdrutvik@gmail.com>
2026-09-18 16:28:11 +0530

    Added Initial project structure and a detect.py to understand how this insightface.app is been used

---
+++ app/detect.py
@@ -0,0 +1,21 @@
+import cv2
+from insightface.app import FaceAnalysis
+
+
+app = FaceAnalysis(name="buffalo_l")
+app.prepare(ctx_id=-1, det_size=(640, 640))
+
+
+image = cv2.imread("image.jpeg")
+
+faces = app.get(image)
+
+print(f"Number of faces detected: {len(faces)}")
+
+for face in faces:
+    print("Bounding box:", face.bbox)
+    print("Confidence:", face.det_score)
+    print("Landmarks:", face.kps)
+
+    print("Embedding shape:", face.embedding.shape)
+    print("First 5 values:", face.embedding[:5])
