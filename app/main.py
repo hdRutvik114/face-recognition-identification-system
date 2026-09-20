8f62e1e8701b61f9d232772764578fd8dfeaf31a
hdRutvik114 <hdrutvik@gmail.com>
2026-09-20 16:11:50 +0530

    feat : Added the identify Endpoint

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import os
 from app.logging_utils import log_message, log_run_start
 from app.service.enrollment_service import EnrollmentService
 
+from app.service.identification_service import IdentificationService
 
 log_run_start()
 
@@ -23,6 +24,8 @@ def root():
     }
 
 
+
+#This is for Enroll 
 @app.post("/enroll")
 async def enroll(
     name: str = Form(...),
@@ -54,3 +57,39 @@ async def enroll(
         for path in image_paths:
             if os.path.exists(path):
                 os.remove(path)
+                
+      
+      
+#This is for the IDENTIFICATION           
+@app.post("/identify")
+async def identify(
+    image: UploadFile = File(...)
+):
+    image_path = None
+
+    try:
+        suffix = os.path.splitext(image.filename)[1]
+
+        log_message(
+            f"Identification request with file '{image.filename}'"
+        )
+
+        with tempfile.NamedTemporaryFile(
+            delete=False,
+            suffix=suffix
+        ) as temp_file:
+
+            temp_file.write(await image.read())
+            image_path = temp_file.name
+        identification_service = IdentificationService()
+        result = identification_service.identify(
+            image_path=image_path
+        )
+
+        log_message(f"Identification result: {result}")
+
+        return result
+
+    finally:
+        if image_path and os.path.exists(image_path):
+            os.remove(image_path)
